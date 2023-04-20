0013101f870f83ea9bc52f0d2498169fbd7e7b92
JuanDBta <juandavid_diaz@hotmail.com>
2023-04-19 20:21:38 -0500

    Fix problems in generating html elements with js

--- JS Files/allprojects_html_elements.js
+++ JS Files/allprojects_html_elements.js
@@ -2,15 +2,6 @@ const projectsContainer = document.querySelector('.projects-container');
 
 const projects = [
   {
-    name: 'Project 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
-    image: 'Images SVG/Projects1.svg',
-    tech: ['HTML/CSS', 'Ruby On Rails', 'JavaScript'],
-    liveLink: 'https://juandbta.github.io/Portfolio-project/',
-    sourceLink: 'https://github.com/JuanDBta/Porfolio-project',
-  },
-
-  {
     name: 'Project 2',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
     image: 'Images SVG/Projects2.svg',
@@ -27,6 +18,7 @@ const projects = [
     liveLink: 'https://juandbta.github.io/Portfolio-project/',
     sourceLink: 'https://github.com/JuanDBta/Porfolio-project',
   },
+
   {
     name: 'Project 4',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.',
@@ -66,14 +58,12 @@ for (let i = 0; i < projects.length; i++) {
   const image = document.createElement('img');
   image.setAttribute('src', project.image);
   backgroundProjects.appendChild(image);
-
-  const goesHereSection = document.createElement('div');
-  goesHereSection.classList.add('goes-here-section');
+  menuProjects.appendChild(backgroundProjects);
 
   const title = document.createElement('h3');
   title.classList.add('projects-title');
   title.innerText = project.name;
-  goesHereSection.appendChild(title);
+  menuProjects.appendChild(title);
 
   const techList = document.createElement('ul');
   techList.classList.add('menu-languages');
@@ -83,7 +73,7 @@ for (let i = 0; i < projects.length; i++) {
     techItem.innerText = project.tech[j];
     techList.appendChild(techItem);
   }
-  goesHereSection.appendChild(techList);
+  menuProjects.appendChild(techList);
 
   const projectButton = document.createElement('button');
   projectButton.classList.add('project-button');
@@ -94,53 +84,7 @@ for (let i = 0; i < projects.length; i++) {
   arrowImg.setAttribute('src', 'Images SVG/ic_arrow_right.svg');
   projectButton.appendChild(arrowImg);
 
-  goesHereSection.appendChild(projectButton);
-
-  menuProjects.appendChild(backgroundProjects);
-  menuProjects.appendChild(goesHereSection);
+  menuProjects.appendChild(projectButton);
 
   projectsContainer.appendChild(menuProjects);
 }
-
-projects.forEach((project) => {
-  const menuProjectsMain = document.createElement('div');
-  menuProjectsMain.classList.add('menu-projects-main');
-
-  const backgroundProjects = document.createElement('div');
-  backgroundProjects.classList.add('background-projects');
-  const image = document.createElement('img');
-  image.src = project.image;
-  backgroundProjects.appendChild(image);
-
-  menuProjectsMain.appendChild(backgroundProjects);
-
-  const goesHereSection = document.createElement('div');
-  goesHereSection.classList.add('goes-here-section');
-
-  const title = document.createElement('h3');
-  title.classList.add('projects-title');
-  title.textContent = project.name;
-  goesHereSection.appendChild(title);
-
-  const list = document.createElement('ul');
-  list.classList.add('menu-languages');
-  project.tech.forEach((language) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('language-button');
-    listItem.textContent = language;
-    list.appendChild(listItem);
-  });
-  goesHereSection.appendChild(list);
-
-  const button = document.createElement('button');
-  button.classList.add('project-button');
-  button.textContent = 'See this project';
-  const arrowImage = document.createElement('img');
-  arrowImage.src = 'Images SVG/ic_arrow_right.svg';
-  button.appendChild(arrowImage);
-  goesHereSection.appendChild(button);
-
-  menuProjectsMain.appendChild(goesHereSection);
-
-  projectsContainer.appendChild(menuProjectsMain);
-});
