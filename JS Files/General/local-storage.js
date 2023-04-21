1f7ca3dfb674de58dc5deed0e1a821de54bcbfa4
JuanDBta <juandavid_diaz@hotmail.com>
2023-04-21 18:03:38 -0500

    Fix ESlint errors

--- JS Files/General/local-storage.js
+++ JS Files/General/local-storage.js
@@ -1,18 +1,18 @@
 function storeFormData() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const comments = document.getElementById('contact-description').value;
-  
-    const formData = {
-      name,
-      email,
-      comments,
-    };
-  
-    localStorage.setItem('formData', JSON.stringify(formData));
-  }
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const comments = document.getElementById('contact-description').value;
 
-  const nameInput = document.getElementById('name');
+  const formData = {
+    name,
+    email,
+    comments,
+  };
+
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+const nameInput = document.getElementById('name');
 const emailInputt = document.getElementById('email');
 const commentsInput = document.getElementById('contact-description');
 
@@ -20,14 +20,15 @@ nameInput.addEventListener('change', storeFormData);
 emailInputt.addEventListener('change', storeFormData);
 commentsInput.addEventListener('change', storeFormData);
 function loadFormData() {
-    const formDataString = localStorage.getItem('formData');
-  
-    if (formDataString) {
-      const formData = JSON.parse(formDataString);
-  
-      document.getElementById('name').value = formData.name;
-      document.getElementById('email').value = formData.email;
-      document.getElementById('contact-description').value = formData.comments;
-    }
+  const formDataString = localStorage.getItem('formData');
+
+  if (formDataString) {
+    const formData = JSON.parse(formDataString);
+
+    document.getElementById('name').value = formData.name;
+    document.getElementById('email').value = formData.email;
+    document.getElementById('contact-description').value = formData.comments;
   }
-  window.addEventListener('load', loadFormData);
+}
+
+window.addEventListener('load', loadFormData);
