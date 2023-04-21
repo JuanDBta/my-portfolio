576ad51d0cbccd0c80bfcb3b34434be68fb2ff33
JuanDBta <juandavid_diaz@hotmail.com>
2023-04-21 13:35:06 -0500

    Validation project ready for review

--- JS Files/General/validation_email.js
+++ JS Files/General/validation_email.js
@@ -1,16 +1,22 @@
 const form = document.querySelector('form');
 const emailInput = document.querySelector('#email');
-const submitBtn = document.querySelector('#contact-button button');
 const errorMsg = document.createElement('p');
-errorMsg.textContent = 'Please enter a lower case email address';
+
+errorMsg.textContent = 'Please enter a lower case email address.';
 errorMsg.classList.add('error-msg');
 errorMsg.style.display = 'none';
+errorMsg.style.color = 'red';
+errorMsg.style.fontFamily = 'Poppins';
+errorMsg.style.fontSize = '3vw';
+errorMsg.style.marginLeft = '5vw';
 emailInput.parentNode.appendChild(errorMsg);
-errorMsg.style.display = 'none';
+
+const errorMsgText = 'Please enter a lower case email address';
+const errorMsgTextDesktop = 'Please enter a lower case email address';
 
 function updateErrorMsgStyle() {
   if (window.matchMedia('(min-width: 768px)').matches) {
-    errorMsg.textContent = errorMsgTextLargeScreen;
+    errorMsg.textContent = errorMsgTextDesktop;
     errorMsg.style.fontSize = '0.8vw';
     errorMsg.style.marginLeft = '1vw';
   } else {
@@ -23,6 +29,7 @@ function updateErrorMsgStyle() {
 updateErrorMsgStyle();
 
 window.addEventListener('resize', updateErrorMsgStyle);
+
 form.addEventListener('submit', (event) => {
   if (emailInput.value !== emailInput.value.toLowerCase()) {
     event.preventDefault();
