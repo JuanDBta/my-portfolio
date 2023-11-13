73063d9650b4a641a52808b076ae63d13fb18b3b
JuanDBta <simplebetsas@gmail.com>
2023-11-13 16:09:33 -0500

    Added event listener to button CV in desktopversion

--- JS Files/Desktop/mainproject_html_elements.js
+++ JS Files/Desktop/mainproject_html_elements.js
@@ -56,3 +56,7 @@ pro.forEach((project) => {
 
   parentElement.appendChild(menuProjectsMain);
 });
+
+document.getElementById('cvButton').addEventListener('click', function() {
+  window.location.href = 'https://docs.google.com/document/d/19hBPTPqCY66v-x-8j0TS23LPBFJDHq1zRRBjqxHEs-E/edit?usp=sharing';
+});
