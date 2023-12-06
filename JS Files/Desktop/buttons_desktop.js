5b41582abf01685b1aacd60f08f37a44ac67581b
JuanDBta <juandavid_diaz@hotmail.com>
2023-12-06 18:22:40 -0500

    Added cards to desktop version

--- JS Files/Desktop/buttons_desktop.js
+++ JS Files/Desktop/buttons_desktop.js
@@ -1,7 +1,14 @@
 function startCollaboration() {
     const contactSection = document.getElementById('Contact');
-    contactSection.scrollIntoView({ behavior: 'auto' });
-}
-
-const startIntroButton = document.getElementById('start-intro');
-startIntroButton.addEventListener('click', startCollaboration);
+    const additionalVW = 7;
+    const additionalPixels = window.innerWidth * (additionalVW / 100);
+    const targetPosition = contactSection.offsetTop - additionalPixels;
+    window.scrollTo({
+      top: targetPosition,
+      behavior: 'auto',
+    });
+  }
+  
+  const startIntroButton = document.getElementById('start-intro');
+  startIntroButton.addEventListener('click', startCollaboration);
+  
